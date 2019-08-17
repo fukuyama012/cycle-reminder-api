912125e16edffa67b2527a0e09ccaa1ddfd39f88
Shingo Fukuyama <fukuyama012@gmail.com>
2019-08-17 20:51:40 +0900

    CIにmigration処理追加

---
+++ db/migrations/20190817201425-create_users.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+CREATE TABLE `users` (
+    `id`            BIGINT UNSIGNED AUTO_INCREMENT,
+    `email`         VARCHAR(255) NOT NULL ,
+    `created_at`    DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    `updated_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uk-email`(`email`)
+) ENGINE=InnoDB;
+
+-- +migrate Down
+DROP TABLE IF EXISTS `users`;
